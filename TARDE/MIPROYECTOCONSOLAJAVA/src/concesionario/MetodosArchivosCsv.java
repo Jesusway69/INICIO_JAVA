c103aa8066d9a6ed3daab69b7f4ca7ac3b252d90
Walter Ismael Sagástegui Lescano <datasagamadrid@gmail.com>
2022-10-19 20:58:36 +0200

    TITULO

--- TARDE/MIPROYECTOCONSOLAJAVA/src/concesionario/MetodosArchivosCsv.java
+++ TARDE/MIPROYECTOCONSOLAJAVA/src/concesionario/MetodosArchivosCsv.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import transacciones.Transaccion;
 
 public class MetodosArchivosCsv {
 
@@ -21,9 +22,18 @@ public class MetodosArchivosCsv {
             f = new File(nra);
             fr = new FileReader(f);
             br = new BufferedReader(fr);
+            int i = 0;
             while ((fila = br.readLine()) != null) {
-                String[] campo = fila.split(";");
-                System.out.println(Arrays.asList(campo));
+                if (i != 0) {
+                    String[] campo = fila.split(";");
+                    //System.out.println(Arrays.asList(campo));
+                    Coche c = new Coche(Integer.parseInt(campo[0]),
+                            campo[1],
+                            campo[2],
+                            Double.parseDouble(campo[3]));
+                    coches_al.add(c);
+                }
+                i++;
             }
         } catch (IOException e) {
             System.out.println("ERROR DE LECTURA");
@@ -31,4 +41,41 @@ public class MetodosArchivosCsv {
         return coches_al;
     }
 
+    public static List<Transaccion> leer1(String nra) {
+        List<Transaccion> transacciones_al = new ArrayList<>();
+
+        String fila = "";
+        File f = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            f = new File(nra);
+            fr = new FileReader(f);
+            br = new BufferedReader(fr);
+            int i = 0;
+            while ((fila = br.readLine()) != null) {
+                if (i != 0) {
+                    String[] campo = fila.split(";");
+                    // System.out.println(Arrays.asList(campo));
+                    Transaccion t = new Transaccion(Integer.parseInt(campo[0]),
+                            campo[1],
+                            campo[2],
+                            Double.parseDouble(campo[3]),
+                            campo[4],
+                            campo[5]);
+                    transacciones_al.add(t);
+                }
+                i++;
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("ERROR DE LECTURA");
+        }
+        return transacciones_al;
+    }
+
 }
+
+    //SELECT COUNT(*) FROM Coche WHERE color = 'verdefor (Coche c : coches_al) {for (Coche c : coches_al) {'
