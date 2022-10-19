da263ea61f2111513f10e3697318154e4f168740
Walter Ismael Sagástegui Lescano <datasagamadrid@gmail.com>
2022-10-19 17:41:25 +0200

    TITULO

--- TARDE/MIPROYECTOCONSOLAJAVA/src/concesionario/Principal.java
+++ TARDE/MIPROYECTOCONSOLAJAVA/src/concesionario/Principal.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class Principal {
 
     public static void main(String[] args) {
+        
         String nra = "datos/concesionario.csv";
+        
         List<Coche> coches_al = MetodosArchivosCsv.leer(nra);
+        
     }
 
 }
