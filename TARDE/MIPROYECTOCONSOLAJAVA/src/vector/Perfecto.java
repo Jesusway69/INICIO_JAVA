a38ee196a7dd393b5b93815a4b1d0b0e9ae9deed
Walter Ismael Sagástegui Lescano <datasagamadrid@gmail.com>
2022-10-21 21:01:32 +0200

    TITULO

--- TARDE/MIPROYECTOCONSOLAJAVA/src/vector/Perfecto.java
+++ TARDE/MIPROYECTOCONSOLAJAVA/src/vector/Perfecto.java
@@ -16,7 +16,17 @@ public class Perfecto {
     }
     
     public static boolean perfecto(int n) {
-        
+        int s = 0;
+        for(int i=1; i<n; i++) {
+            if(n%i==0) {
+                s = s + i;
+            }
+        }
+        if(n == s) {
+            return true;
+        }else {
+            return false;
+        }
     }
     
     
